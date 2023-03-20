088e4edad3b1174d9317d4a60db397500031cfdd
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-03-20 08:21:37 -0700

    done

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-let randomizeButton = document.getElementById("randomizeColor");
-let output = document.getElementById("colorOutput");
-randomizeButton.addEventListener("click", () => {
+let button = document.getElementById("button")
+let output = document.getElementById("output")
 
-let redValue = Math.ceil(Math.random() * 9)
-let greenValue = Math.ceil(Math.random() * 9)
-let blueValue = Math.ceil(Math.random() * 9)
+function randInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
-output.setAttribute("style", `background-color: rgb(120, 56, 255)`)
+function randRGB() {
+    return `rgb(${randInt(255)}, ${randInt(255)}, ${randInt(255)})`;
+}
 
+button.addEventListener("click", () => {
+    output.style.background = randRGB();
 })
+
+// button.addEventListener("click", () => {
+
+// let redValue = Math.ceil(Math.random() * 255)
+// let greenValue = Math.ceil(Math.random() * 255)
+// let blueValue = Math.ceil(Math.random() * 255)
+
+// console.log(redValue, greenValue, blueValue)
+// output.setAttribute("style", `background-color: rgb(${redValue}, ${greenValue}, ${blueValue})`)
+
+// })
