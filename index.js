bb1a02537d230c51cf5a6b8e1264679bb4ce5519
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-03-20 08:03:46 -0700

    dsfds

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let start = document.getElementById("startButton");
-let intro = document.getElementById("intro");
-start.addEventListener("click", () => {
-	intro.classList.add("introGone");
-});
+// let start = document.getElementById("startButton");
+// let intro = document.getElementById("intro");
+// start.addEventListener("click", () => {
+// 	intro.classList.add("introGone");
+// });
+let body = document.getElementById("body")
+
+let boardDiv = document.querySelectorAll(".board")
+boardDiv.addEventListener("click", () => {
+	alert("Perry The Platypus")
+})
